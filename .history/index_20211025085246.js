2d2927a584f4fd00738bb73b6e3156eea258aeed
josceno <id629631@outlook.com>
2021-10-25 09:55:57 -0300

    site1

---
+++ .history/index_20211025085246.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/',function(req,res){
+    res.sendfile(path.join(___dirname +'/index.html'));
+})
+
+
+app.use('/',router)/
+app.listen(process.env.port || 300);
---
+++ .history/index_20211025091528.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+"indexS.hml"));
+})
+
+app.use('/',router)/
+app.listen(process.env.port || 3000);
+
+console.log("servidor rodando!")
